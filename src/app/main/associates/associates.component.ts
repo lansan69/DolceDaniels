af066a34f56fccc94e975ed9be477df4d939db46
lansan69 <alanisg029@gmail.com>
2025-06-14 04:46:47 -0600

    third section almost ended and modifications to about us

---
+++ src/app/main/associates/associates.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-associates',
+  imports: [],
+  templateUrl: './associates.component.html',
+  styleUrl: './associates.component.scss'
+})
+export class AssociatesComponent {
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ProductsComponent } from "./products/products.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutUsComponent, ProductsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
