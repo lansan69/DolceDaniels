2494c65692a3f148eb876e87ae443898805330d0
lansan69 <alanisg029@gmail.com>
2025-06-27 20:59:26 -0600

    Finishing shop and part setting associates

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { ProductsComponent } from "./products/products.component";
+import { ShopComponent } from "./shop/shop.component";
+import { AssociatesComponent } from "./associates/associates.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent, AboutUsComponent, ProductsComponent],
+  imports: [HeroComponent, AboutUsComponent, ProductsComponent, ShopComponent, AssociatesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
--- src/app/main/shop/shop.component.ts
+++ src/app/main/shop/shop.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ShopComponent {
 
+  activeSection: 'cremas' | 'jugos' = 'cremas'; // default
+
+  setActive(section: 'cremas' | 'jugos') {
+    this.activeSection = section;
+  }
+
 }
