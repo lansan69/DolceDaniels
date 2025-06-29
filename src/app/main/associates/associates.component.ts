b24c74e7dc794e37cfbaa7986158056d8e768fd3
lansan69 <alanisg029@gmail.com>
2025-06-29 14:40:13 -0600

    safe copy

--- src/app/main/associates/associates.component.ts
+++ src/app/main/associates/associates.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-associates',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './associates.component.html',
   styleUrl: './associates.component.scss'
 })
