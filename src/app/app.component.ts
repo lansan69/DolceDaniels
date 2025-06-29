bc73798a23a043a7cb241e8555f14adac316190b
lansan69 <alanisg029@gmail.com>
2025-06-29 15:30:12 -0600

    Footer advance and shop fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "./main/main.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', '../styles.scss'],
-  imports: [MainComponent]
+  imports: [MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'dolcePage';
